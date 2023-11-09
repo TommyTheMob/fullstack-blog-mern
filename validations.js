04885c34d38460903bc101a6f57d92857a512c13
TommyTheMob <zv3rbrus@gmail.com>
2023-11-09 21:13:06 +0300

    edit adding post validation messages

--- validations.js
+++ validations.js
@@ -13,8 +13,8 @@ export const registerValidation = [
 ]
 
 export const postCreateValidation = [
-    body('title', 'Введите заголовок статьи').isLength({ min: 3 }).isString(),
-    body('text', 'Введите текст статьи').isLength({ min: 10 }).isString(),
+    body('title', 'Введите заголовок статьи (не менее трех символов)').isLength({ min: 3 }).isString(),
+    body('text', 'Введите текст статьи (не менее 10 символов)').isLength({ min: 10 }).isString(),
     body('tags', 'Неверный формат тэгов (укажите массив)').optional().isArray(),
     body('imageUrl', 'Неверная ссылка на изображение').optional().isString(),
 ]
